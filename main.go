0c3e4800c624ed46a2cfa873f949f162c51d3192
Your Name <tmsoft12@gmail.com>
2024-07-08 04:00:15 +0500

    a

--- main.go
+++ main.go
@@ -12,23 +12,18 @@ import (
 )
 
 func main() {
-	// Veritabanı bağlantısını başlat
+
 	config.InitDatabase()
 	defer config.DB.Close()
 
-	// Fiber uygulamasını oluştur
 	app := fiber.New()
 
-	// Middleware'leri uygula
 	app.Use(logger.New())
 	app.Use(middleware.CookieMiddleware)
 
-	// Kontrolcü veritabanı bağlantısını ayarla
 	dbController := &controller.Database{DB: config.DB}
 
-	// Rotaları yapılandır
 	router.SetupRoutes(app, dbController)
 
-	// Fiber uygulamasını dinle
-	log.Fatal(app.Listen(":3001"))
+	log.Fatal(app.Listen(":3000"))
 }
